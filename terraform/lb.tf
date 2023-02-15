d76d28125d88987e24e6b1c49220322ab41099fb
Alexander-High <waymaster@list.ru>
2023-02-15 04:39:11 +0300

    terraform lb reconf

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -26,13 +26,12 @@ resource "yandex_lb_network_load_balancer" "lb" {
 resource "yandex_lb_target_group" "lb" {
   name = "lb-target-group"
 
-  target {
-    subnet_id = var.subnet_id
-    address   = yandex_compute_instance.app.*.network_interface.0.ip_address
-  }
-  target {
-    subnet_id = var.subnet_id
-    address   = yandex_compute_instance.app.*.network_interface.0.ip_address
+  dynamic "target" {
+    for_each = yandex_compute_instance.app.*.network_interface.0.ip_address
+    content {
+      subnet_id = var.subnet_id
+      address   = target.value
+    }
   }
 
 }
