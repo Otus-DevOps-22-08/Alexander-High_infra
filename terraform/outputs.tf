212b92f18a85b4566f59fe11a4e98918ef83093a
Alexander-High <waymaster@list.ru>
2023-02-15 05:22:32 +0300

    terraform bugfix2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "internal_ip_address_reddit-app" {
-  value = ["${yandex_compute_instance.app.*.network_interface.0.ip_address}"]
+  value = "${yandex_compute_instance.app.*.network_interface.0.ip_address}"
 }
 
 output "external_ip_address_reddit_app" {
-  value = ["${yandex_compute_instance.app.*.network_interface.0.nat_ip_address}"]
+  value = "${yandex_compute_instance.app.*.network_interface.0.nat_ip_address}"
 }
 
 output "external_ip_address_lb" {
-  value = ["${yandex_lb_network_load_balancer.lb.*}"]
+    value = [for s in yandex_lb_network_load_balancer.lb.listener: s.external_address_spec.*.address].0
 }
