0a88f264f8f61d5cbe052089bdf8b3457a5f80b0
Alexander-High <waymaster@list.ru>
2023-02-15 05:29:32 +0300

    terraform bugfix4

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
-      version = "0.80.0"
+      version = "0.96.0"
     }
   }
   required_version = ">= 0.13"
